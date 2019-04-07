0e127d6e84e3402c2f2a2757143d4276651f33cf
nikhil <rose12nikhil@gmail.com>
2019-04-07 19:12:28 +0530

    remove logs

--- src/App.js
+++ src/App.js
@@ -20,21 +20,17 @@ class App extends Component {
 
     onDragEnd = (result) => {
         if (!result.destination) {
-            console.log("destination empty");
             return
         }
 
         // console.log("debug:result: ", result)
-        let {destination, source, draggableId} = result;
+        let {destination} = result;
 
-        draggableId = draggableId.split('-')[1];
         // console.log("draggableId : ", draggableId )
 
         const newState = {...this.state};
 
-        const SourceColumn = newState.columns[source.droppableId.split('-')[1]];
         const destinationColumn = newState.columns[destination.droppableId.split('-')[1]];
-        console.log("newState.selectedTasks: ", newState.selectedTasks.map(t => t.taskId))
 
         const selectedTaskIds = newState.selectedTasks.map(t => t.taskId);
 
@@ -44,26 +40,23 @@ class App extends Component {
             newState.selectedTasks.forEach(task => {
                 const column = newState.columns[task.columnId];
                 const index = column.taskIds.indexOf(task.taskId);
-                console.log('')
                 if (index > -1)
                     column.taskIds.splice(index, 1);
             })
-        })()
-        console.log("SourceColumn: ", SourceColumn, destination.index)
+        })();
         destinationColumn.taskIds.splice(destination.index, 0, ...selectedTaskIds);
-        console.log("newState:start: ", this.state)
         newState.selectedTasks = [];
         this.setState({
             ...newState
-        }, () => console.log("newState:end: ", this.state))
+        }, )
 
-    }
+    };
     handleDragUpdate = () => {
-        console.log("call: handleDragUpdate ")
-    }
+        //console.log("call: handleDragUpdate ")
+    };
 
     onDragStart = (start) => {
-        console.log("onDragStart : ", start)
+        //console.log("onDragStart : ", start)
         const id = start.draggableId;
         const selected = this.state.selectedTasks.find(
             (taskId) => taskId === id,
@@ -72,7 +65,7 @@ class App extends Component {
         // if dragging an item that is not selected - unselect all items
         if (!selected) {
             // this.unselectAll();
-            console.log("NO SELECTED: ")
+            //console.log("NO SELECTED: ")
         }
 
         this.setState({
@@ -81,11 +74,11 @@ class App extends Component {
 
     };
     changeSelection = ({taskId, columnId}, isMultiple) => {
-        console.log("taskID, isMultiple: ", taskId, columnId)
+        //console.log("taskID, isMultiple: ", taskId, columnId)
         const {selectedTasks} = {...this.state};
 
-        const wasSelected = selectedTasks.some(task => task.taskId === taskId)
-        console.log("wasSelected : ", wasSelected)
+        const wasSelected = selectedTasks.some(task => task.taskId === taskId);
+        //console.log("wasSelected : ", wasSelected)
         const newTaskIds = (() => {
             if (wasSelected) {
                 selectedTasks
@@ -104,7 +97,7 @@ class App extends Component {
         this.setState({
             selectedTasks: newTaskIds
         })
-    }
+    };
 
     render() {
         return (
@@ -120,7 +113,7 @@ class App extends Component {
                         .map(columnId => {
 
                             const column = this.state.columns[columnId];
-                            const tasks = column.taskIds.map(id => this.state.tasks[id])
+                            const tasks = column.taskIds.map(id => this.state.tasks[id]);
                             return <Column
                                 selectionCount={this.state.selectedTasks.length}
                                 selectedTasks={this.state.selectedTasks}
